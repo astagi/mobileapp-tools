04053ae7ece37da27b63197b1bbbf71f30c67c0b
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-05-07 02:15:52 +0200

    Upload files for TurpialTutorial2

---
+++ TurpialTutorial2/assets/www/js/templates.js
@@ -0,0 +1,32 @@
+window.templates = function() {
+    var templates = {};
+	
+    function checkTemplate(filename, callback){
+        if(templates[filename])
+            return true;
+        else
+            return false;
+    };
+	
+    function getTemplate(filename, callback){
+        if(templates[filename])
+            callback(templates[filename]);
+        else
+            $.get("templates/status.partial", function(response) {
+                templates[filename] = response;
+                callback(response);
+            });
+    };
+	
+    function fillTemplate(template, values){
+        for (var key in values)
+            template = template.replace("<% @" + key + " %>", values[key]);
+        return template;
+    };
+
+    return {
+        getTemplate: getTemplate,
+        checkTemplate: checkTemplate,
+        fillTemplate: fillTemplate
+    };
+}();
