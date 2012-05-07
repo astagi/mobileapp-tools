288221a576b4469beb468a13a7ddd1d09697de30
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-05-07 02:46:19 +0200

    Added remaining values in the template and added comments

--- TurpialTutorial2/assets/www/js/templates.js
+++ TurpialTutorial2/assets/www/js/templates.js
@@ -1,6 +1,7 @@
 window.templates = function() {
     var templates = {};
 	
+    //Check if a template is just in RAM
     function checkTemplate(filename, callback){
         if(templates[filename])
             return true;
@@ -8,6 +9,7 @@ window.templates = function() {
             return false;
     };
 	
+    //Get a template from RAM if present, from local otherwise
     function getTemplate(filename, callback){
         if(templates[filename])
             callback(templates[filename]);
@@ -18,6 +20,7 @@ window.templates = function() {
             });
     };
 	
+    //Fill a template with specified values
     function fillTemplate(template, values){
         for (var key in values)
             template = template.replace("<% @" + key + " %>", values[key]);
