d05044c9a2bc660c1667539f47abf4af72c797e4
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-05-16 03:16:46 +0200

    Just defined a pattern for scripts

--- ridtabs.sh
+++ ridtabs.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+# Author: Andrea Stagi (4ndreaSt4gi)
+# Description: converts tabs in 4 spaces in every source file. Use it before commit!
+# License: GPL v3
+
 for fname in `find . -type f -regex ".*\.\(js\|java\|html\|xml\)"`
 do
     expand -t 4 $fname > aux.txt
     cat aux.txt > $fname
 done
-
 rm aux.txt
